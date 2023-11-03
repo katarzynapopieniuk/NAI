c733bb76642f7e1892eacc323abf4a4ca8ba265c
Katarzyna Popieniuk <katarzynapopieniuk@gmail.com>
2023-11-03 17:22:33 +0100

    zjazd 3 # show recommendations

--- ZJAZD3/TopRecomendations.py
+++ ZJAZD3/TopRecomendations.py
@@ -55,8 +55,23 @@ def avg_scores(dataset, n, films):
         if i >= n:
             break
 
-    return scores2
+    avg_scores = {}
+    for key, value in scores2.items():
+        avg_scores[key] = scores2[key]["sum"] / scores2[key]["count"]
 
+    return avg_scores
+
+def get_recommended(sorted_average_scores, n, user_data):
+    recommended = []
+    count = 0
+    for key, value in sorted_average_scores.items():
+        if key not in user_data:
+            recommended.append(key)
+            count += 1
+        if count >= n:
+            break
+
+    return recommended
 
 if __name__ == '__main__':
     args = build_arg_parser().parse_args()
@@ -68,12 +83,19 @@ if __name__ == '__main__':
     with open(ratings_file, 'r', encoding='utf-8') as f:
         data = json.loads(f.read())
 
-    print("\nScore:")
-
     for item in data:
         if item != user1:
             scores[item] = euclidean_score(data, user1, item)
 
     sorted_scores = OrderedDict(sorted(scores.items(), key=itemgetter(1), reverse=True))
-    print(sorted_scores)
-    print(avg_scores(sorted_scores, 3, data))
+
+    average_scores = avg_scores(sorted_scores, 3, data)
+    sorted_average_scores = OrderedDict(sorted(average_scores.items(), key=itemgetter(1), reverse=True))
+    recommended = get_recommended(sorted_average_scores, 5, data[user1])
+    print("Rekomendujemy: ")
+    print(recommended)
+    not_recommended = get_recommended(OrderedDict(sorted(average_scores.items(), key=itemgetter(1), reverse=False)), 5, data[user1])
+    print("Odradzamy: ")
+    print(not_recommended)
+
+
